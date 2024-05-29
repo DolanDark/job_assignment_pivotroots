c8d8fec86e75f263c49b87371802fad09b73257a
DolanDark <77528341+DolanDark@users.noreply.github.com>
2024-05-29 13:25:27 +0530

    added homepage with route list

--- pivotroots_frontend/src/App.js
+++ pivotroots_frontend/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import UserPage from './pages/UserPage'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
 	  <div>
 		<BrowserRouter>
+				<Route path="/" exact component={HomePage} />
 				<Route path="/login" exact component={Login} />
 				<Route path="/register" exact component={Register} />
 				<Route path="/userpage" exact component={UserPage} />
